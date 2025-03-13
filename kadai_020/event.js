c663b77842d5c16ff5ce3ed704b6fa2b33ad519c
honda-desu <h9.k.honda@gmail.com>
2025-03-13 09:25:23 +0900

    kadai_020wo

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  const textElement = document.getElementById('text');
+  textElement.textContent = 'ボタンがクリックされました！';
+});
